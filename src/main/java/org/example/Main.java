cd25d1eacb698364f33f67c2cf15ac3be5c08f61
Nihad <nihad.uddin@ippen-digital.de>
2023-10-24 10:12:07 +0200

    init database

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+
+    static final String JDBC_Driver ="org.h2.Driver";
+    static final String DB_url = "jdbc:h2:./src/main/java/db/carsharing";
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+
+        try{
+            Class.forName(JDBC_Driver);
+
+            System.out.println("Connecting to database...");
+            connection = DriverManager.getConnection(DB_url);
+
+            System.out.println("Creating table in database...");
+            statement = connection.createStatement();
+            String sql = "create table if not exists COMPANY(" +
+                    "ID INTEGER," +
+                    "NAME VARCHAR(20)" +
+                    ");";
+
+            statement.executeUpdate(sql);
+
+            connection.close();
+            statement.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
